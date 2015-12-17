5e42fb1f969cb7620bb56d420951258ad616d243
Alexsjones <tibbar@freedommail.ch>
2015-12-17 10:30:10 +0000

    building out protocol

--- src/protocol/wpprotocol.c
+++ src/protocol/wpprotocol.c
@@ -2,8 +2,35 @@
 *     File Name           :     /home/anon/Code/whisper-protocol/src/protocol/wpprotocol.c
 *     Created By          :     anon
 *     Creation Date       :     [2015-12-10 14:38]
-*     Last Modified       :     [AUTO_UPDATE_BEFORE_SAVE]
+*     Last Modified       :     [2015-12-17 10:30]
 *     Description         :      
 **********************************************************************************/
 
 #include "wpprotocol.h"
+#include <jnxc_headers/jnxcheck.h>
+#include <jnxc_headers/jnxguid.h>
+#include <jnxc_headers/jnxlog.h>
+
+wp_state wpprotocol_generate_message(Wpmessage **message, char *sender, 
+    char *recipient) {
+  JNXCHECK(sender);
+  JNXCHECK(recipient);
+
+  wpmessage__init(*message);
+ 
+  jnx_guid g;
+  jnx_guid_create(&g);
+ 
+  char *idstr;
+  jnx_guid_to_string(&g, &idstr);
+  int len = strlen(idstr);
+  JNXLOG(LDEBUG,"Creating message identity => %s", idstr);
+  (*message)->id = malloc(len + 1);
+  memcpy((*message)->id,idstr,len);
+  
+
+
+  free(idstr);
+
+  return E_WP_OKAY;
+} 
--- src/protocol/wpprotocol.h
+++ src/protocol/wpprotocol.h
@@ -2,12 +2,32 @@
 *     File Name           :     src/protocol/wpprotocol.h
 *     Created By          :     anon
 *     Creation Date       :     [2015-12-10 14:36]
-*     Last Modified       :     [2015-12-10 14:38]
+*     Last Modified       :     [2015-12-17 10:12]
 *     Description         :      
 **********************************************************************************/
 
 #ifndef __WPPROTOCOL_H__
 #define __WPPROTOCOL_H__
 #include "wpmessage.pb-c.h"
+typedef enum wp_state {
+  E_WP_OKAY,
+  E_WP_FAIL,
+  E_WP_UNKNOWN
+}wp_state;
 
+typedef enum data_type {
+  E_DT_ASYMKEY,
+  E_DT_SYMKEY,
+  E_DT_CHAR,
+  E_DT_INT
+}data_type;
+
+
+wp_state wpprotocol_generate_message(Wpmessage **message, char *sender, 
+    char *recipient); 
+
+wp_state wpprotocol_generate_action(Wpaction **action);
+
+wp_state wpprotocol_generate_contextdata(Wpcontextdata **context, void *data, data_type type);
 #endif
+
