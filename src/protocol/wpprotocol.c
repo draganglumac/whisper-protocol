f2980e99a09950b688c6dad2eb591bf71e0e0eeb
Alexsjones <tibbar@freedommail.ch>
2015-12-17 13:22:05 +0000

    adding in test harness

--- src/protocol/wpprotocol.c
+++ src/protocol/wpprotocol.c
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*     File Name           :     /home/anon/Code/whisper-protocol/src/protocol/wpprotocol.c
-*     Created By          :     anon
-*     Creation Date       :     [2015-12-10 14:38]
-*     Last Modified       :     [2015-12-17 10:30]
-*     Description         :      
-**********************************************************************************/
+ *     File Name           :     /home/anon/Code/whisper-protocol/src/protocol/wpprotocol.c
+ *     Created By          :     anon
+ *     Creation Date       :     [2015-12-10 14:38]
+ *     Last Modified       :     [2015-12-17 13:15]
+ *     Description         :      
+ **********************************************************************************/
 
 #include "wpprotocol.h"
 #include <jnxc_headers/jnxcheck.h>
@@ -17,17 +17,24 @@ wp_state wpprotocol_generate_message(Wpmessage **message, char *sender,
   JNXCHECK(recipient);
 
   wpmessage__init(*message);
- 
+
   jnx_guid g;
   jnx_guid_create(&g);
- 
+
   char *idstr;
   jnx_guid_to_string(&g, &idstr);
   int len = strlen(idstr);
   JNXLOG(LDEBUG,"Creating message identity => %s", idstr);
   (*message)->id = malloc(len + 1);
   memcpy((*message)->id,idstr,len);
-  
+
+  len = strlen(sender);
+  (*message)->sender = malloc(len + 1);
+  memcpy((*message)->sender,sender,len);
+
+  len = strlen(recipient);
+  (*message)->recipient = malloc(len + 1);
+  memcpy((*message)->recipient,recipient,len);
 
 
   free(idstr);
